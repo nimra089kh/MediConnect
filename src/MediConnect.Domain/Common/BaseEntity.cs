4b6746291b768648f85b2ff2f31b23b3670c9466
nimra089kh <164199675+nimra089kh@users.noreply.github.com>
2026-05-01 19:27:11 +0500

    feat: add BaseEntity and domain enums

---
+++ src/MediConnect.Domain/Common/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediConnect.Domain.Common
+{
+	internal class BaseEntity
+	{
+	}
+}
---
+++ src/MediConnect.Domain/Enums/BloodGroup.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediConnect.Domain.Enums
+{
+	internal class BloodGroup
+	{
+	}
+}
